30f1a48b85c61d561e4a6d59d5cb92013afa4f95
Ksandros <ksandros-x@yandex.ru>
2021-01-13 21:03:40 +0500

    Add water row

--- Board.cpp
+++ Board.cpp
@@ -8,6 +8,7 @@ void Board::createBoard(std::istream& in)
 	string line;
 	while (getline(in, line)) {
 		vector<shared_ptr<Cell>> row;
+		row.push_back(make_shared<Water>(WATER));
 		for (auto c : line) {
 			shared_ptr<Cell> cell;
 			if (c == WATER) {
@@ -18,7 +19,14 @@ void Board::createBoard(std::istream& in)
 			}
 			row.push_back(cell);
 		}
+		row.push_back(make_shared<Water>(WATER));
+		cells.push_back(row);
 	}
+
+	vector<shared_ptr<Cell>> row(cells[0].size(), 
+		make_shared<Water>(WATER));//констуктор вектора, количество элементов, элементы
+	cells.insert(cells.begin(), row);
+	cells.push_back(row);
 }
 
 bool Board::isBoardCorrect() const
